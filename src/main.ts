610b0a05addb140331f237b899f0880d372b3eb1
Diego Fabbri <diegofabbri.dev@gmail.com>
2024-07-11 16:50:42 -0300

    feat: update app port

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://task-management-frontend-five.vercel.app/',
-    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Authorization', 'Content-Type'],
     credentials: true,
   });
-  await app.listen(443);
+  await app.listen(5000);
 }
 bootstrap();
